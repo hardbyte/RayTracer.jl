6a5e43558398356bc4fb90aecf438c043d69c76f
Brian Thorne <brian@thorne.link>
2019-12-22 21:12:02 +1300

    Tracing some rays, hitting some spheres

--- RayTracer.jl
+++ RayTracer.jl
@@ -1,10 +1,11 @@
 module RayTracer
 using ColorTypes
+import LinearAlgebra: norm, dot
 
 to_uint = x -> UInt8(round(x * 255))
 color_to_vector = c::RGB -> [red(c), green(c), blue(c)]
 
-function output_as_ppm(data::Array{RGB{Float64}, 2}, fname="out.ppm")
+function output_as_ppm(data::AbstractArray{RGB{Float64}, 2}, fname="out.ppm")
     rows, columns = size(data)
     println("Writing data as ppm")
     println("Input dimensions: $(size(data))")
@@ -12,7 +13,7 @@ function output_as_ppm(data::Array{RGB{Float64}, 2}, fname="out.ppm")
         write(f, "P3\n")
         write(f, "$columns $rows\n")
         write(f, "255\n")
-        for row in 1:rows
+        for row in rows:-1:1
             for column in 1:columns
                 r, g, b = map(to_uint, color_to_vector(data[row, column]))
                 write(f, "$r $g $b\n")
@@ -20,4 +21,65 @@ function output_as_ppm(data::Array{RGB{Float64}, 2}, fname="out.ppm")
         end
     end
 end
+
+# Ray
+struct Ray
+    origin::Vector{Float64}
+    direction::Vector{Float64}
+end
+
+function point_along_ray(ray::Ray, time::Float64)
+    return ray.origin + time*ray.direction
+end
+
+struct HitRecord
+    t::Float64
+    p::Vector{Float64}
+    normal::Vector{Float64}
+end
+
+# function hit(ray::Ray, t_min::Float64, t_max::Float64, record::hit_record)
+#     return 0
+# end
+
+struct Sphere
+    center::Vector{Float64}
+    radius::Float64
+end
+
+function hit(obj::Sphere, ray::Ray, t_min::Float64, t_max::Float64)
+    oc = ray.origin - obj.center
+    a = dot(ray.direction, ray.direction)
+    b = dot(oc, ray.direction)
+    c = dot(oc, oc) - obj.radius^2
+    discriminant = b^2 - a * c
+    if discriminant > 0
+        temp = (-b - sqrt(discriminant))/a
+        if (temp < t_max) && (temp > t_min)
+            p = point_along_ray(ray, temp)
+            return HitRecord(temp, p, (p - obj.center) / obj.radius)
+        end
+        temp = (-b + sqrt(discriminant))/a
+        if (temp < t_max) && (temp > t_min)
+            p = point_along_ray(ray, temp)
+            return HitRecord(temp, p, (p - center) / obj.radius)
+        end
+    end
+end
+
+function hit(objects::Array{Sphere}, ray::Ray, t_min::Float64, t_max::Float64)
+    hit_anything = false
+    closest_so_far = t_max
+    closest_hit = Nothing
+    for obj in objects
+        optional_hit_record = hit(obj, ray, t_min, t_max)
+        if typeof(optional_hit_record) == HitRecord
+            hit_anything = true
+            closest_so_far = optional_hit_record.t
+            closest_hit = optional_hit_record
+        end
+    end
+    return closest_hit
+end
+
 end # module
--- main.jl
+++ main.jl
@@ -1,32 +1,108 @@
 using ColorTypes
+using Images
+import LinearAlgebra: norm, dot
+
+# We use image data H,W,C
+# https://juliaimages.org/latest/quickstart/
+
+# TODO: drop the include?
 include("RayTracer.jl")
 
 import Base.zero
 zero(::Type{RGB{Float64}}) = RGB{Float64}(0,0,0)
 
-#img_data = rand(RGB{Float64}, 100, 200)
-#RayTracer.output_as_ppm(img_data)
-
-function matrix_to_rgb_image(img_data::Array{Float64,3})::Array{RGB{Float64}, 2}
-    rows, cols = size(img_data)
-    img::Array{RGB{Float64}, 2} = zeros(RGB{Float64}, rows, cols)
-
+function hit_sphere(center::Vector{Float64}, radius::Float64, ray::RayTracer.Ray)::Float64
+    oc = ray.origin - center
+    a = dot(ray.direction, ray.direction)
+    b = 2.0 * dot(oc, ray.direction)
+    c = dot(oc, oc) - radius^2
+    discriminant = b^2 - 4*a*c
+    if discriminant < 0
+        return -1.0
+    else
+        return (-b - sqrt(discriminant))/(2.0*a)
+    end
 end
 
-function generate_test_image()
-    rows, cols = 100, 200
-    channels = 3 # RGB
-    img::Array{Float64, 3} = zeros(Float64, rows, cols, channels)
+unit_vector = v::Vector -> v/norm(v)
 
-    for row in rows:-1:1
-        for col in 1:cols
-            img[row, col, :] = [col/cols, row/rows, 0.2]
+function color(ray::RayTracer.Ray, objects::Array{RayTracer.Sphere})
+
+    rec = RayTracer.hit(objects, ray, 0.0, maxintfloat(Float64))
+
+    if typeof(rec) == RayTracer.HitRecord
+        N = rec.normal
+        return 0.5 * (N .+ 1)
+    end
+
+    # Linear Interpolation of blue to white along y axis.
+    unit_direction = unit_vector(ray.direction)
+    t = 0.5 * (unit_direction[2] + 1.0)
+    return (1.0 - t) * [1.0, 1.0, 1.0] + t * [0.5, 0.7, 1.0]
+end
+
+
+function generate_image_from_ray_tracer()
+    # We use image data H,W,C
+    # https://juliaimages.org/latest/quickstart/
+    channels, height, width = 3, 100, 200
+    # Preallocate output image array
+    img_CHW::Array{Float64, 3} = zeros(Float64, channels, height, width)
+    lower_left_corner = [-2.0, -1.0, -1.0]
+    horizontal = [4.0, 0.0, 0.0]
+    vertical = [0.0, 2.0, 0.0]
+    origin = [0.0, 0.0, 0.0]
+
+    for row in height:-1:1
+        for col in 1:width
+            u = col/width
+            v = row/height
+            ray = RayTracer.Ray(origin, lower_left_corner + u*horizontal + v*vertical)
+            img_CHW[:, row, col] = color(ray)
         end
     end
-    return img
+    return img_CHW
 end
 
-img_data = generate_test_image()
-@show typeof(img_data)
-rgb_img = matrix_to_rgb_image(img_data)
+function ray_trace_sphere_objects()
+    # We use image data H,W,C
+    # https://juliaimages.org/latest/quickstart/
+    channels, height, width = 3, 100, 200
+    # Preallocate output image array
+    img_CHW::Array{Float64, 3} = zeros(Float64, channels, height, width)
+    lower_left_corner = [-2.0, -1.0, -1.0]
+    horizontal = [4.0, 0.0, 0.0]
+    vertical = [0.0, 2.0, 0.0]
+    origin = [0.0, 0.0, 0.0]
+
+    scene_objects = [
+        RayTracer.Sphere([0.0, -100.5, -1.0], 100.0),
+        RayTracer.Sphere([-0.5, 0.0, -1.0], 0.5),
+        RayTracer.Sphere([0.5, 0.0, -1.0], 0.5),
+    ]
+
+    for row in height:-1:1
+        for col in 1:width
+            u = col/width
+            v = row/height
+            ray = RayTracer.Ray(origin, lower_left_corner + u*horizontal + v*vertical)
+            p = RayTracer.point_along_ray(ray, 2.0)
+            img_CHW[:, row, col] = color(ray, scene_objects)
+        end
+    end
+    return img_CHW
+end
+
+
+img_data = ray_trace_sphere_objects()
+
+# For rendering we use Image data as RGB array.
+# Note our 3 dim matrix use channel-height-width order
+# Use channelview and colorview to convert
+rgb_img = colorview(RGB, img_data)
+
+@show typeof(rgb_img)
+@show size(rgb_img)
+
 RayTracer.output_as_ppm(rgb_img)
+
